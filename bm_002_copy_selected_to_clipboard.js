a502afa8028ce7b9e6d97fe5208efe42a353fdb5
ricketts30 <ricketts30@yahoo.com>
2018-10-13 17:59:08 +0100

    adding the selection devmarklet

--- bm_002_copy_selected_to_clipboard.js
+++ bm_002_copy_selected_to_clipboard.js
@@ -1,1 +1,16 @@
-alert("This bookmarklet 002 was able to run.");
+let selected =            
+document.getSelection().rangeCount > 0
+  ? document.getSelection().getRangeAt(0)
+  : false;        
+console.log(selected);    
+let body = document.getElementsByName("body")[0];
+let text_area = document.createElement('textarea');
+text_area.value = "hello world";
+body.appendChild(text_area);
+text_area.select();
+text_area.document.execCommand('copy');
+body.removeChild(text_area);
+if (selected) {                                
+    document.getSelection().removeAllRanges();    
+    document.getSelection().addRange(selected);   
+  }
