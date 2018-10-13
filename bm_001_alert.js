15b658c27bbbc916e8a972e94a75316ae1b0cafe
ricketts30 <ricketts30@yahoo.com>
2018-10-13 18:01:51 +0100

    attempting to fix the reload issue

--- bm_001_alert.js
+++ bm_001_alert.js
@@ -1,1 +1,2 @@
+console.log("This bookmarklet was able to run.");
 alert("This bookmarklet was able to run.");
--- bm_002_copy_selected_to_clipboard.js
+++ bm_002_copy_selected_to_clipboard.js
@@ -1,10 +1,10 @@
-let selected =            
+var selected =            
 document.getSelection().rangeCount > 0
   ? document.getSelection().getRangeAt(0)
   : false;        
 console.log(selected);    
-let body = document.getElementsByName("body")[0];
-let text_area = document.createElement('textarea');
+var body = document.getElementsByName("body")[0];
+var text_area = document.createElement('textarea');
 text_area.value = "hello world";
 body.appendChild(text_area);
 text_area.select();
