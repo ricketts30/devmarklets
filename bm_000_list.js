8017a73688d49b90b33c0e7caea2c7ace9216aad
Howard Ricketts <molorosh@gmail.com>
2018-10-13 17:20:07 +0100

    dynamically create the bookmarklet links

--- bm_000_list.js
+++ bm_000_list.js
@@ -1,7 +1,23 @@
 var bm_list = {
-    "items": [
-        { "script" : "bm_001_alerts.js" , "title": "DVM:Alert", "about": "just runs a simple alert popup to see if your website allows bookmarklets" },
-        { "script" : "bm_002_copy_selected_to_clipboard.js" , "title": "DVM:CopySelected", "about": "just copies the currently selected text to the clipboard" },
-        { "script" : "bm_003_click_info.js" , "title": "DVM:Explore", "about": "presents short summary information about the element you click on next" },
-    ]
+    "items": 
+        [
+            { 
+                "script" : "bm_001_alerts.js", 
+                "code" : "001",
+                "title": "DVM:Alert", 
+                "about": "just runs a simple alert popup to see if your website allows bookmarklets"
+            },
+            { 
+                "script" : "bm_002_copy_selected_to_clipboard.js",
+                "code" : "002",
+                "title": "DVM:CopySelected", 
+                "about": "just copies the currently selected text to the clipboard" 
+            },
+            { 
+                "script" : "bm_003_click_info.js", 
+                "code": "003",
+                "title": "DVM:Explore", 
+                "about": "presents short summary information about the element you click on next" 
+            },
+        ]
 };
