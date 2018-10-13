40db8e79a14a89a307e707a69bf728cee5001119
ricketts30 <ricketts30@yahoo.com>
2018-10-13 18:04:25 +0100

    still trying to resolve reload issue

--- bm_002_copy_selected_to_clipboard.js
+++ bm_002_copy_selected_to_clipboard.js
@@ -3,13 +3,12 @@ document.getSelection().rangeCount > 0
   ? document.getSelection().getRangeAt(0)
   : false;        
 console.log(selected);    
-var body = document.getElementsByName("body")[0];
 var text_area = document.createElement('textarea');
 text_area.value = "hello world";
-body.appendChild(text_area);
+document.body.appendChild(text_area);
 text_area.select();
 text_area.document.execCommand('copy');
-body.removeChild(text_area);
+document.body.removeChild(text_area);
 if (selected) {                                
     document.getSelection().removeAllRanges();    
     document.getSelection().addRange(selected);   
